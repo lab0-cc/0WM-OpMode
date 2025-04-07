6d7d3d7c66aae94d02474a22c2ec691a6d5ebf01
Benjamin Somers <benjamin.somers@imt-atlantique.fr>
2025-04-07 14:49:00 +0200

    Add tabbing component
    Signed-off-by: Benjamin Somers <benjamin.somers@imt-atlantique.fr>

---
+++ js/components/tab-container.mjs
@@ -0,0 +1,90 @@
+// This module implements a tab container
+
+import { DynamicShadow, Stylable } from '/js/mixins.mjs';
+
+
+class TabContainer extends DynamicShadow(Stylable(HTMLElement)) {
+    #currentTab;
+    #tabs;
+    #submitBtn;
+
+    constructor() {
+        super();
+        this.addStylesheet('components/tab-container.css');
+        this.#currentTab = null;
+        this.#tabs = [];
+        this.addHandler(e => {
+            if (e.classList.contains('tab')) {
+                e.addEventListener('click', () => this.selectTab(e));
+                if (this.#currentTab === null)
+                    this.selectTab(e);
+                e.ok = true;
+                this.#tabs.push(e);
+                this.#refreshTabState(e, document.getElementById(e.dataset.target));
+            }
+            else if (e.classList.contains('submit')) {
+                this.#submitBtn = e;
+            }
+        });
+
+        document.addEventListener('statuschange', e => this.#refreshPaneState(e.target));
+    }
+
+    // Select the given tab
+    selectTab(e) {
+        if (this.#currentTab !== null) {
+            this.#currentTab.classList.remove('selected');
+            document.getElementById(this.#currentTab.dataset.target).style.removeProperty('visibility');
+        }
+        this.#currentTab = e;
+        this.#currentTab.classList.add('selected');
+        document.getElementById(this.#currentTab.dataset.target).style.visibility = 'visible';
+    }
+
+    // Explore a pane and update the tab state
+    #refreshPaneState(e) {
+        let refreshedTab = null;
+        let pane = null;
+        for (const tab of this.#tabs) {
+            const node = document.getElementById(tab.dataset.target);
+            if (node.contains(e)) {
+                refreshedTab = tab;
+                pane = node;
+                break;
+            }
+        }
+
+        this.#refreshTabState(refreshedTab, pane);
+    }
+
+    // Update the tab state given its pane
+    #refreshTabState(tab, pane) {
+        tab.classList.remove('warning', 'error');
+        switch (Math.max(...[...pane.querySelectorAll('*')].map(e => e.getStatus?.() ?? 0))) {
+            case 1:
+                tab.classList.add('warning');
+                tab.ok = false;
+                break;
+            case 2:
+                tab.classList.add('error');
+                tab.ok = false;
+                break;
+            default:
+                tab.ok = true;
+        }
+
+        if (this.#tabs.every(e => e.ok))
+            this.#submitBtn?.removeAttribute('disabled');
+        else
+            this.#submitBtn?.setAttribute('disabled', 'disabled');
+    }
+}
+
+
+try {
+    customElements.define('tab-container', TabContainer);
+}
+catch (e) {
+  if (!(e instanceof DOMException))
+    throw e;
+}
